9b95bad51030e56e9ff2dfd14d62ca0ce8c8ae15
chenly11109 <brittneycavanesspub88@gamil.com>
2024-08-11 21:53:50 +0800

    fix: resume dynamic import

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,8 @@
-import ResumeContainer from "./components/ResumeContainer";
+import dynamic from "next/dynamic";
 
+const ResumeContainer = dynamic(() => import("./components/ResumeContainer"), {
+  ssr: false,
+});
 export default function Resume() {
   return (
     <div className="w-screen h-screen overflow-hidden">
