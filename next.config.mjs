528929bd3947ba6c87c8297bfcceac907f0f6ae4
chenly11109 <brittneycavanesspub88@gamil.com>
2024-08-13 00:07:26 +0800

    fix: nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/home/projects",
+    output: "export",
+    reactStrictMode: true,
 
     images: {
         remotePatterns: [
